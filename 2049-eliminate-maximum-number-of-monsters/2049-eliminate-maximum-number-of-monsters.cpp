f6f952a2bba888dad3ebe9ee7003f6aa5ee157ed
Aditya Roushan <138475855+Aditya6688@users.noreply.github.com>
2023-11-07 21:54:36 +0530

    Time: 111 ms (54.67%), Space: 83.2 MB (86.67%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
         int n = dist.size();
-        std::vector<double> time_to_city(n);
+        std::vector<int> time_to_city(n);
         
         for (int i = 0; i < n; ++i) {
-            time_to_city[i] = static_cast<double>(dist[i]) / speed[i];
+            time_to_city[i] = (dist[i]-1) / speed[i];
         }
         
         std::sort(time_to_city.begin(), time_to_city.end());
         
         for (int i = 0; i < n; ++i) {
-            if (time_to_city[i] <= i) {
+            if (time_to_city[i] < i) {
                 return i;
             }
         }
