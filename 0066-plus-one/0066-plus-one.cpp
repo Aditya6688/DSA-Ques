3a9a3b4d8acd1e995f3cdbed29cc147dab0d2c5a
Aditya Roushan <138475855+Aditya6688@users.noreply.github.com>
2025-08-17 14:22:06 +0530

    Time: 0 ms (100%), Space: 11.6 MB (5.48%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-    int n = digits.size();
-    for (int i = n - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
+        int n = digits.size();
+        int carry = 1;  // because we want to add 1
+
+        for (int i = n - 1; i >= 0 && carry; i--) {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
         }
-        digits[i] = 0;
-    }
-    digits.insert(digits.begin(), 1);
-    return digits;
+
+        if (carry) {  // if still carry remains, e.g., 999 + 1 = 1000
+            digits.insert(digits.begin(), carry);
+        }
+
+        return digits;
     }
 };
