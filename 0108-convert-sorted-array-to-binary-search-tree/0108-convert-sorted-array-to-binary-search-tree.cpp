61d626ac43d895550fcd1bdb334ffa313c659a2d
Aditya Roushan <138475855+Aditya6688@users.noreply.github.com>
2025-08-19 22:03:35 +0530

    Time: 1 ms (76.76%), Space: 23.1 MB (23.21%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -11,20 +11,16 @@
  */
 class Solution {
 public:
-    TreeNode* func(vector<int>& nums, int i, int j){
-        if (i > j) return NULL;
-        
-        int m = (i+j)/2;
-        TreeNode* root = new TreeNode(nums[m]);
-
-        root->left = func(nums,i,m-1);
-        root->right = func(nums,m+1,j);
-
+    TreeNode* build(vector<int>& nums, int l, int r) {
+        if (l > r) return nullptr;
+        int mid = l + (r - l) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = build(nums, l, mid - 1);
+        root->right = build(nums, mid + 1, r);
         return root;
     }
+    
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        int n = nums.size();
-        TreeNode* root = func(nums,0,n-1);
-        return root;
+        return build(nums, 0, nums.size() - 1);
     }
 };
