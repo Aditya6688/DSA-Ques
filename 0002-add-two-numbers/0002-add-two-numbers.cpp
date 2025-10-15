0d1589e4d175f4a01989461847202b8c37431c2d
Aditya Roushan <138475855+Aditya6688@users.noreply.github.com>
2025-10-15 16:28:05 +0530

    Time: 0 ms (100%), Space: 77.5 MB (13.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,41 +12,38 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         if(!l1 && !l2) return nullptr;
-        if(!l1) return l2;
-        if(!l2) return l1;
-
-        vector<int> v;
+        vector<int> res;
         int carry = 0;
 
         while(l1 && l2){
-            int sum = l1->val+l2->val+carry;
-            v.push_back(sum%10);
+            int sum = l1->val + l2->val + carry;
+            res.push_back(sum%10);
             carry = sum/10;
             l1 = l1->next;
             l2 = l2->next;
         }
 
         while(l1){
-            int sum = l1->val+carry;
-            v.push_back(sum%10);
+            int sum = l1->val + carry;
+            res.push_back(sum%10);
             carry = sum/10;
             l1 = l1->next;
         }
 
         while(l2){
-            int sum = l2->val+carry;
-            v.push_back(sum%10);
+            int sum = l2->val + carry;
+            res.push_back(sum%10);
             carry = sum/10;
             l2 = l2->next;
         }
+        if(carry) res.push_back(carry);
 
-        if(carry>0) v.push_back(carry);
-
-        ListNode* head = new ListNode(v[0]);
-        ListNode* temp = head;
-        for(int i=1;i<v.size();i++){
-            temp->next = new ListNode(v[i]);
-            temp = temp->next;
+        
+        ListNode* head = new ListNode(res[0]);
+        ListNode* curr = head;
+        for(int i=1;i<res.size();i++){
+            curr->next = new ListNode(res[i]);
+            curr = curr->next;
         }
 
         return head;
