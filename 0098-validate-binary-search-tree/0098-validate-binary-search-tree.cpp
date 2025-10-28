cc3600f50160b8b34ecda6d6ad7abffa58f0bb85
Aditya Roushan <138475855+Aditya6688@users.noreply.github.com>
2025-10-28 15:10:29 +0530

    Time: 0 ms (100%), Space: 21.9 MB (46.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,18 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool func(TreeNode* root, long long mini, long long maxi){
-        if(!root) return true;
-        
-        return (root->val>mini && root->val<maxi) && func(root->left, mini, root->val) && func(root->right, root->val, maxi);
-    }
+    bool func(TreeNode* root, long long mini, long long maxi) {
+        if (!root)
+            return true;
 
-    bool isValidBST(TreeNode* root) {
-        return func(root, LLONG_MIN, LLONG_MAX);
-    }
-};
+        return (root->val > mini && root->val < maxi) &&
+            (func(root->left, mini, root->val)) &&
+            (func(root->right, root->val, maxi));
+    } 
+    bool isValidBST(TreeNode* root) { 
+        return func(root, LLONG_MIN, LLONG_MAX); 
+    } 
+}; 
