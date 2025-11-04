76fa486f12b734c8f296adffaab386769619597c
Aditya Roushan <138475855+Aditya6688@users.noreply.github.com>
2025-11-04 15:49:57 +0530

    Time: 6 ms (32.06%), Space: 27 MB (91.83%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -6,32 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* func(vector<int>& inorder, vector<int>& postorder, int is, int ie, int &pi){
-        if(is>ie) return NULL;
-        TreeNode* curr = new TreeNode(postorder[pi--]);
+    TreeNode* func(vector<int>& inorder, vector<int>& postorder, int is, int ie, int &pi) {
+        int ind = 0;
+        if (is > ie)
+            return nullptr;
 
-        int inind;
-        for(int i=is;i<=ie;i++){
-            if(inorder[i] == curr->val){
-                inind = i;
+        TreeNode* root = new TreeNode(postorder[pi--]);
+
+        for (int i = is; i <= ie; i++) {
+            if (inorder[i] == root->val) {
+                ind = i;
                 break;
             }
         }
 
-        curr->right = func(inorder, postorder, inind+1, ie, pi);
-        curr->left = func(inorder, postorder, is, inind-1,pi);
+        root->right = func(inorder, postorder, ind + 1, ie,pi);
+        root->left = func(inorder, postorder, is, ind - 1,pi);
 
-        return curr;
+        return root;
     }
-
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int pi = postorder.size()-1;
-        TreeNode* res = func(inorder, postorder, 0,inorder.size()-1,pi);
-        return res;
+        int pi = inorder.size()-1;
+        int m = inorder.size();
+        return func(inorder,postorder,0,m-1,pi);
     }
 };
